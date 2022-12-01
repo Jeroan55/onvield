ba4a5c3ad40bddddcc6844cfceeea3ba5da53bc1
notgood54 <84709584+notgood54@users.noreply.github.com>
2022-12-01 15:07:02 +0700

    halamansewa1

--- app/View/Components/Header.php
+++ app/View/Components/Header.php
@@ -6,6 +6,7 @@ use Illuminate\View\Component;
 
 class Header extends Component
 {
+    public $title;
     /**
      * Create a new component instance.
      *
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -15,10 +15,10 @@
 
 
 
-    <title>$title</title>
+    <title>{{$title}}</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
         <div class="container">
             <a class="navbar-brand fs-1" href="#">Gor Sampel</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -30,20 +30,20 @@
                     <a class="nav-link" href="#">HOME</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="#">DAFTAR LAPANGAN</a>
+                    <a class="nav-link" href="#daftarLapangan">DAFTAR LAPANGAN</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="#">SEWA</a>
+                    <a class="nav-link" href="/sewa">SEWA</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="#">KONTAK</a>
+                    <a class="nav-link" href="#contact">KONTAK</a>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
 
-    <div class="isi">
+    <div class="isi container-fluid">
         {{$slot}}
     </div>
 
